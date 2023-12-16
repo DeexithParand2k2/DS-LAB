02e58f823cac987c11e57e8a42c3e170910d05ca
DeexithParand2k2 <deexith2002@gmail.com>
2023-12-16 19:50:10 +0530

    first commit

---
+++ LinearandLogistic/linearandlogistic.R
@@ -0,0 +1,10 @@
+dataset = read.csv("data-marketing-budget-12mo.csv", header=T, colClasses = c("numeric", "numeric", "numeric"))
+head(dataset,5)
+simple.fit = lm(Sales~Spend,data=dataset)
+summary(simple.fit)
+multi.fit = lm(Sales~Spend+Month, data=dataset)
+summary(multi.fit)
+input<- mtcars [,c("am","cyl","hp","wt")]
+print(head(input))
+am.data =glm(formula = am ~ cyl+hp+wt,data = input,family = binomial)
+print(summary(am.data))
